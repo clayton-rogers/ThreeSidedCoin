07ac092e1a706e8cd2fc178ba572b1f659e8deb5
claytonrogers <claytonrogers53@gmail.com>
2018-01-31 00:13:08 -0500

    Kind of working

--- ThreeSidedCoin/main.cpp
+++ ThreeSidedCoin/main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include <cmath>
 #include <random>
+#include <map>
+#include <string>
 
 const double PI = 3.141592653589793238462643383279502884197169399375105820974944592;
 const double G = 9.80665; // m/s^2
 const double DENSITY = 1225.0; // kg/m^3
+const double COEFFICIENT_RESTITUTION = 1.0; // ND 
 const double RADIUS = 0.03; // m = 3 cm
 const double INITIAL_HEIGHT = 0.2; // m = 20 cm
-const bool VERBOSE = true;
+const bool   VERBOSE = true;
+
+void log(std::string message) {
+	if (VERBOSE) {
+		std::cout << message << std::endl;
+	}
+}
 
 double radians(double degrees) {
 	return degrees / 180.0 * PI;
@@ -70,7 +79,7 @@ public:
 		// angle can actually be plus or minus this amout
 		std::uniform_int_distribution<> intdist(0, 1);
 		int sign = intdist(mt);
-		if (sign = 1) {
+		if (sign == 1) {
 			angle *= -1.0;
 		}
 		return angle;
@@ -81,20 +90,45 @@ public:
 		return RADIUS;
 	}
 
+	void logTotalEnergy() {
+		log("Total energy: " + std::to_string(PE + KE + RE));
+	}
+
 	void collide() {
 		// randomly choose the angle that the coin impacts at
 		double alpha = getRandomAlpha();
+		alpha = theta;
 
+		logTotalEnergy();
 		// position the coin at the required height for the edge to touch
 		// calculate the new KE and PE that this would entail
-		double height = centerToEdge * std::cos(alpha - theta);
+		double height = centerToEdge * std::cos(std::abs(alpha) - theta);
 		double newPE = G * height * mass;
 		double deltaPE = PE - newPE;
 		KE += deltaPE;
 		PE = newPE;
+		logTotalEnergy();
+		
 
-		// for now, elastic collision
+		// Find impulse
+		double velocity = std::sqrt(2.0 * KE / mass);
+		double impulse = 2.0 * COEFFICIENT_RESTITUTION * mass * velocity;
+		
+		
+		// Rotation
+		double omega = std::sqrt(2.0 * RE / momentI);
+		double delta_omega = impulse * centerToEdge / momentI * std::sin(std::abs(alpha) - theta);
+		if (alpha < 0) { delta_omega *= -1.0; }
 
+		double new_omega = omega + delta_omega;
+		RE = 0.5 * momentI * new_omega * new_omega;
+
+		// Linear
+		double delta_velocity = impulse / mass * std::cos(std::abs(alpha) - theta);
+		double new_vel = velocity - delta_velocity;
+		KE = 0.5 * mass * new_vel * new_vel;
+
+		logTotalEnergy();
 	}
 
 	
